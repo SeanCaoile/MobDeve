b502fcb5d4e0d65845629badc9bfa69721839196
Thalyonn <thalyonns@gmail.com>
2023-10-17 18:24:15 +0800

    Models And Generator

---
+++ app/src/main/java/com/mobdeve/s13/caoile/sean/mc0/RecipeModel.kt
@@ -0,0 +1,18 @@
+package com.mobdeve.s13.caoile.sean.mc0;
+
+class RecipeModel(ingredients: ArrayList<IngredientModel>, recipeName: String, instructions: String, creator: String, imageId : Int) {
+    var ingredients = ingredients
+        private set
+
+    var recipeName = recipeName
+        private set
+
+    var instructions = instructions
+        private set
+
+    var creator = creator
+        private set
+
+    var imageId = imageId
+        private set
+}
