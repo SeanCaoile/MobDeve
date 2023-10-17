ef3c9440a1321c078315d87925dd7b61d94b1bb1
Thalyonn <thalyonns@gmail.com>
2023-10-17 15:07:19 +0800

    Added Recipe

---
+++ app/src/main/java/com/mobdeve/s13/caoile/sean/mc0/RecipeFragment.kt
@@ -0,0 +1,17 @@
+package com.mobdeve.s13.caoile.sean.mc0
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RecipeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe, container, false)
+    }
+}
