775927cac53d1f7fe2ceb065db213e44c35b0927
Thalyonn <thalyonns@gmail.com>
2023-10-18 15:59:55 +0800

    Added Back Button

--- app/src/main/java/com/mobdeve/s13/caoile/sean/mc0/RecipeActivity.kt
+++ app/src/main/java/com/mobdeve/s13/caoile/sean/mc0/RecipeActivity.kt
@@ -1,9 +1,11 @@
 package com.mobdeve.s13.caoile.sean.mc0
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -22,6 +24,7 @@ class RecipeActivity : AppCompatActivity()  {
     private lateinit var food_creatorTv: TextView
     private lateinit var instructionsTv: TextView
     private lateinit var recipeImg: ImageView
+    private lateinit var backBtn: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe)
@@ -30,11 +33,27 @@ class RecipeActivity : AppCompatActivity()  {
         food_creatorTv = findViewById<View>(R.id.food_creatorTv) as TextView
         instructionsTv = findViewById<View>(R.id.instructionsTv) as TextView
         recipeImg = findViewById<View>(R.id.recipeImg) as ImageView
+        backBtn = findViewById<View>(R.id.backBtn) as ImageButton
 
         foodNameTv.text = intent.getStringExtra(RecipeActivity.NAME_KEY)
         food_creatorTv.text = intent.getStringExtra(RecipeActivity.CREATOR_KEY)
         instructionsTv.text = intent.getStringExtra(RecipeActivity.INSTRUCTIONS_KEY)
         recipeImg.setImageResource(intent.getIntExtra(RecipeActivity.IMG_KEY, 0))
+
+        backBtn.setOnClickListener(View.OnClickListener {
+            /*  TODO:
+             *      1. Declare a new Intent
+             *      2. Place the position into the Intent
+             *      3. Set the result as OK passing the intent
+             *      4. Properly finish the activity
+             *      NOTE: We're passing back the position as we need to know what to delete in
+             *            our data / ArrayList
+             * */
+
+            finish()
+
+
+        })
         
 
     }
