e338833fbc8cfa84dacc26221d124c67c12b6186
SeanCaoile <sean_iverson_caoile@dlsu.edu.ph>
2023-10-20 12:54:44 +0800

    fixed hamburgerMenu

--- app/src/main/java/com/mobdeve/s13/caoile/sean/mc0/HomePage.kt
+++ app/src/main/java/com/mobdeve/s13/caoile/sean/mc0/HomePage.kt
@@ -5,6 +5,7 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
