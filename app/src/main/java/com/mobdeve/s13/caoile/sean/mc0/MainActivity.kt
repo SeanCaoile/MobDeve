ef13d61610db14a88e31287f4a0f484016f81f81
Thalyonn <thalyonns@gmail.com>
2023-11-07 19:07:54 +0800

    Connected

--- app/src/main/java/com/mobdeve/s13/caoile/sean/mc0/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s13/caoile/sean/mc0/MainActivity.kt
@@ -2,9 +2,11 @@ package com.mobdeve.s13.caoile.sean.mc0
 
 import android.app.Activity
 import android.content.ContentProviderClient
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -15,6 +17,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
@@ -24,7 +28,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val db = Firebase.firestore
+        Log.i(TAG, "STARTING DB CONTENT CHECK")
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.i(TAG, "Checking users")
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+
         auth = FirebaseAuth.getInstance()
+
+
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
